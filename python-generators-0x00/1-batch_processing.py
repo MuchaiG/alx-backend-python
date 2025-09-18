da77eb4d1a4046b05dfa79c9ca139942f32335b3
MuchaiG <dennisgachuru2@gmail.com>
2025-09-18 16:10:30 +0300

    Fixing errors

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -4,6 +4,7 @@ import mysql.connector
 def stream_users_in_batches(batch_size):
     
     # fetch rows from user_data table in batches.
+    # Yields lists of rows (each up to batch_size).
     
     connection = mysql.connector.connect(
         host="localhost",
@@ -14,27 +15,31 @@ def stream_users_in_batches(batch_size):
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
 
-    while True: # loop 1
+    while True:  
         batch = cursor.fetchmany(batch_size)
         if not batch:
             break
-        yield batch
+        yield batch   
 
     cursor.close()
     connection.close()
+    return   
 
 
 def batch_processing(batch_size):
-    
+
     # process batches of users and yield only users older than 25.
-    for batch in stream_users_in_batches(batch_size):  # loop 2
-        filtered = [user for user in batch if int(user["age"]) > 25]  # loop 3
+
+    for batch in stream_users_in_batches(batch_size): 
+        filtered = [user for user in batch if int(user["age"]) > 25]
         yield filtered
+
+    return
 
 
 # Test run
 if __name__ == "__main__":
-    print("📌 Processing users in batches:")
     for users in batch_processing(3):
-        if users:  
+        if users:
             print(users)
+
