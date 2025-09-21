a44f34ab8c2f5be3ca5be4bc11d787dce82959d5
MuchaiG <dennisgachuru2@gmail.com>
2025-09-21 08:46:49 +0300

    Fix: errors

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,24 +1,28 @@
 import sqlite3
 import functools
+from datetime import datetime   
 
 #### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # extract query argument
+        # Extract query from arguments
         query = None
         if args:   # check positional args
             query = args[0]
         elif "query" in kwargs:  # check keyword arg
             query = kwargs["query"]
 
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         if query:
-            print(f"[LOG] Executing SQL query: {query}")
+            print(f"[{timestamp}] Executing SQL query: {query}")
         else:
-            print("[LOG] No query provided")
+            print(f"[{timestamp}] No query provided")
 
         return func(*args, **kwargs)
     return wrapper
+
 
 @log_queries
 def fetch_all_users(query):
@@ -29,6 +33,8 @@ def fetch_all_users(query):
     conn.close()
     return results
 
+
 #### fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
 print(users)
+
